7f21080a43eb140f8e559089df01c13e08a66197
Moyinaiyeniko <42965174+Moyinaiyeniko@users.noreply.github.com>
2018-10-30 03:22:48 -0700

    Update q3.js

--- q3.js
+++ q3.js
@@ -1,5 +1,9 @@
 $('#add').click(function(){
 var textFieldValue = $('#textfield').val();
+if ( textFieldValue){
 $('#list').append('<li>'+ textFieldValue + '</li>');
+}
+else{
+window.alert("Please fill in the field");
+}
 });
-
