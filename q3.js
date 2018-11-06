47fca7c83bd9a5811f9bae48f0b0f0455c6d0796
Moyinaiyeniko <42965174+Moyinaiyeniko@users.noreply.github.com>
2018-11-06 06:13:42 -0800

    Delete q3.js

--- q3.js
+++
@@ -1,9 +0,0 @@
-$('#add').click(function(){
-var textFieldValue = $('#textfield').val();
-if ( textFieldValue){
-$('#list').append('<li>'+ textFieldValue + '</li>');
-}
-else{
-window.alert("Please fill in the field");
-}
-});
